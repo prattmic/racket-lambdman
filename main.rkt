2978433939dbdffa8f479c45c9913b61477c280b
Michael Pratt <michael@pratt.im>
2014-12-05 22:45:40 -0500

    Reindent file
    I think this is closer to idiomatic Racket.

--- main.rkt
+++ main.rkt
@@ -4,27 +4,28 @@
 (require racket/file)
 
 (define world-file
-    (command-line
-     #:program "lambdaman"
-     #:args (filename) ; expect one argument, the filename
-     filename)) ; return the filename
+  (command-line
+    #:program "lambdaman"
+    #:args (filename) ; expect one argument, the filename
+    filename)) ; return the filename
 
 (define (world-char->world-symbol world-char)
-    (match world-char
-        [#\space 'empty]
-        [#\# 'wall]
-        [#\. 'pill]
-        [#\o 'power-pill]
-        [#\% 'fruit]
-        [#\\ 'lambda-man]
-        [#\= 'ghost]))
+  (match world-char
+    [#\space 'empty]
+    [#\# 'wall]
+    [#\. 'pill]
+    [#\o 'power-pill]
+    [#\% 'fruit]
+    [#\\ 'lambda-man]
+    [#\= 'ghost]))
 
 (define (read-world-map)
-    (define lines (file->lines world-file))
-    (filter (negate empty?) ; remove empty lines
-      (map
-        (lambda (line) (map world-char->world-symbol (string->list line)))
-        lines)))
+  (define lines (file->lines world-file))
+  (filter (negate empty?) ; remove empty lines
+          (map (lambda (line) ; convert line to world symbols
+                       (map world-char->world-symbol
+                            (string->list line)))
+               lines)))
 
 (define world-map (read-world-map))
 
